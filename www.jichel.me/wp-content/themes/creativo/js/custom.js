c48b76de0594d8b415ebe909295442fc24916cc9
abdulhadi25 <abdulhadiaamir25@gmail.com>
2020-09-08 15:16:36 +0500

    Jichel Portfolio
    Testing ...

---
+++ www.jichel.me/wp-content/themes/creativo/js/custom.js
@@ -0,0 +1,136 @@
+//NIKE
+$('figure:nth-child(1)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#bc2b26");
+    
+})
+
+// Lovely Beards
+
+$('figure:nth-child(2)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#161616");
+    
+})
+
+// INGUAT
+
+$('figure:nth-child(3)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#40DFD0");
+    
+})
+
+// Sound Asleep 
+
+$('figure:nth-child(4)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#f00");
+    
+})
+
+// Johnny Cupcake
+
+$('figure:nth-child(5)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#72dfb8");
+    
+})
+
+// Intergem
+
+$('figure:nth-child(6)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#FFC000");
+    
+})
+
+// Sock it
+
+$('figure:nth-child(7)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#EB550D");
+    
+})
+
+// Maximus Sound
+
+$('figure:nth-child(8)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#FF9D00");
+    
+})
+
+// Webheay
+
+$('figure:nth-child(9)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#645081");
+    
+})
+
+// Doyon
+
+$('figure:nth-child(10)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#006FC0");
+    
+})
+
+// AIA
+
+$('figure:nth-child(11)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#ff0000");
+    
+})
+
+// Green Bakery
+
+$('figure:nth-child(12)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#F494AC");
+    
+})
+
+// Seeking health
+
+$('figure:nth-child(13)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#00AF50");
+    
+})
+
+// Economad
+
+$('figure:nth-child(14)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#8875AC");
+    
+})
+
+// Wonderwarm
+
+$('figure:nth-child(15)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#DAA8BA");
+    
+})
+
+// Crystal 
+
+$('figure:nth-child(16)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#4E236C");
+    
+})
+
+// vape
+
+$('figure:nth-child(17)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#E2E2E4");
+    
+})
+
+// Lavelle
+
+$('figure:nth-child(18)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#D17E06");
+    
+})
+
+// The Cyclist
+
+$('figure:nth-child(19)').on('mouseenter',function(){
+    $('#za_custom_script').css( "background", "#e34690");
+    
+})
+// Default 
+
+$('figure').on('mouseleave',function(){
+    $('#za_custom_script').css( "background-color", "black");
+})
